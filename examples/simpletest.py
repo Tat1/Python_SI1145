35fe510eb82056c4b79ad1150455b6aa0456493a
Tat1 <33688075+Tat1@users.noreply.github.com>
2019-01-22 15:53:08 +0000

    Making compatible with Python 3
    Made it compatible by adding the parenthesis to the print to allow the error message of python 3 to be removed.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -44,16 +44,16 @@ import SI1145.SI1145 as SI1145
 
 sensor = SI1145.SI1145()
 
-print 'Press Cntrl + Z to cancel'
+print('Press Cntrl + Z to cancel')
 
 while True:
         vis = sensor.readVisible()
         IR = sensor.readIR()
         UV = sensor.readUV()
         uvIndex = UV / 100.0
-        print 'Vis:             ' + str(vis)
-        print 'IR:              ' + str(IR)
-        print 'UV Index:        ' + str(uvIndex)
+        print('Vis:             ' + str(vis))
+        print('IR:              ' + str(IR))
+        print('UV Index:        ' + str(uvIndex))
 
         time.sleep(3)
 
